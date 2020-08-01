55ecff65a5720c0de390ca3ce46cf293cd118e58
pedroleon21 <52296989+pedroleon21@users.noreply.github.com>
2020-08-01 14:32:58 -0300

    Add files via upload

---
+++ HelloWorldAppConsole/Mundo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorldAppConsole
+{
+    public class Mundo
+    {
+        public int Num { get; set; }
+        public string Resposta { set; get; }
+        public Mundo ()
+        { }
+        public Mundo(string resposta)
+        {
+            Resposta = resposta;
+        }
+        public Mundo(string resposta, int num)
+        {
+            Resposta = resposta;
+            Num = num;
+        }
+        public bool Valido()
+        {
+            Console.WriteLine($"{this.Resposta.Length} {this.Num}");
+            if (Resposta.Length <= 0 || Num == 0)
+                return false;
+            return true;
+        }
+
+    }
+}
---
+++ HelloWorldAppConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorldAppConsole
+{
+    class Program
+    {
+        static void Main()
+        {
+            Mundo a = new Mundo("resposta padrao", 10);
+            string g;
+            Console.WriteLine($"{a.Valido()} , {a.Num} , {a.Resposta}");
+            g = Console.ReadLine();
+            Console.WriteLine(g); 
+        }
+    }
+}
